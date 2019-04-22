1d5493fffd52e30265bc7214cec9aaf38f99f4bf
Герман <buhrus2@yandex.ru>
2019-04-22 21:21:15 +0400

    function fix

--- money.py
+++ money.py
@@ -1,15 +1,17 @@
 import json
 import urllib.request
 
-page = urllib.request.urlopen('https://www.cbr-xml-daily.ru/daily_json.js')
-data = json.loads(page.read())
-valutes = data['Valute']
+def get_max_currency():
+    page = urllib.request.urlopen('https://www.cbr-xml-daily.ru/daily_json.js')
+    data = json.loads(page.read())
+    valutes = data['Valute']
+    maxVal = 0
+    maxValName = ''
+    for name in valutes:
+        if valutes[name]['Value'] > maxVal:
+            maxVal = valutes[name]['Value']
+            maxValName = valutes[name]['Name']
+    return maxValName
 
 
-maxVal = 0
-maxValName = ''
-for name in valutes:
-    if valutes[name]['Value'] > maxVal:
-        maxVal = valutes[name]['Value']
-        maxValName = valutes[name]['Name']
-print(maxValName)
+print(get_max_currency())
